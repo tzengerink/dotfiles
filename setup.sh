c44c6d54bc586f3181bb4f7da518b02547a1f93d
T. Zengerink <t.zengerink@gmail.com>
2012-01-10 11:21:43 +0100

    modified setup script

--- setup.sh
+++ setup.sh
@@ -4,59 +4,15 @@
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-# Bash files
-
-if [ -f ~/.bash_profile ]; then
-	mv ~/.bash_profile ~/.bash_profile~
-	echo -e "Backed up: .bash_profile"
-fi
-ln -s "$DIR/.bash_profile" ~/.bash_profile
-echo -e "Link to .bash_profile create"
-
-if [ -f ~/.bashrc ]; then
-	mv ~/.bashrc ~/.bashrc~
-	echo -e "Backed up: .bashrc"
-fi
-ln -s "$DIR/.bashrc" ~/.bashrc
-echo -e "Link to .bashrc create"
-
-if [ -f ~/.bash_aliases ]; then
-	mv ~/.bash_aliases ~/.bash_aliases~
-	echo -e "Backed up: .bash_aliases"
-fi
-ln -s "$DIR/.bash_aliases" ~/.bash_aliases
-echo -e "Link to .bash_aliases create"
-
-
-# Vim files
-
-if [ -f ~/.vimrc ]; then
-	mv ~/.vimrc ~/.vimrc~
-	echo -e "Backed up: .vimrc"
-fi
-ln -s "$DIR/.vimrc" ~/.vimrc
-echo -e "Link to .vimrc create"
-
-if [ -d ~/.vim ]; then
-	mv ~/.vim ~/.vim~
-	echo -e "Backed up: .vim"
-fi
-ln -s "$DIR/.vim" ~/.vim
-echo -e "Link to .vim create"
-
-
-# Git files
-
-if [ -d ~/.gitconfig ]; then
-	mv ~/.gitconfig ~/.gitconfig~
-	echo -e "Backed up: .gitconfig"
-fi
-ln -s "$DIR/.gitconfig" ~/.gitconfig
-echo -e "Link to .gitconfig create"
-
-if [ -d ~/.gitignore ]; then
-	mv ~/.gitignore ~/.gitignore~
-	echo -e "Backed up: .gitignore"
-fi
-ln -s "$DIR/.gitignore" ~/.gitignore
-echo -e "Link to .gitignore create"
+# Loop and check if directory or file exists
+# Backup if so, then create symlink
+for FILE in `find $DIR -name ".*" -mindepth 1`
+do
+	BASENAME=$(basename "$FILE")
+	if [ -f ~/$BASENAME ] || [ -d ~/$BASENAME ]; then
+		mv ~/$BASENAME ~/$BASENAME~
+		echo -e "Backup for $BASENAME created"
+	fi
+	ln -s $FILE ~/$BASENAME
+	echo -e "Link for $BASENAME created"
+done
