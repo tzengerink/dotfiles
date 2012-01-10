712843096eb257ff9555bc82f300b38015201de7
T. Zengerink <t.zengerink@gmail.com>
2012-01-10 18:07:23 +0100

    edited setup

--- setup.sh
+++ setup.sh
@@ -6,13 +6,11 @@ DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Loop and check if directory or file exists
 # Backup if so, then create symlink
-for FILE in `find $DIR -name ".*" -mindepth 1`
-do
+for FILE in $DIR/.{bashrc,bash_profile,exports,aliases,functions,vimrc,vim,gitconfig,gitignore}; do
 	BASENAME=$(basename "$FILE")
 	if [ -f ~/$BASENAME ] || [ -d ~/$BASENAME ]; then
 		mv ~/$BASENAME ~/$BASENAME~
 		echo -e "Backup for $BASENAME created"
 	fi
 	ln -s $FILE ~/$BASENAME
-	echo -e "Link for $BASENAME created"
 done
