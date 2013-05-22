757602ae4af69f468b1fe0db504ce85ff5b0a215
T. Zengerink <t.zengerink@gmail.com>
2013-05-22 16:32:30 +0200

    minor configuration changes

--- .bin/weather.py
+++ .bin/weather.py
@@ -11,15 +11,16 @@
     Licensed under MIT License
     See: https://raw.github.com/gist/3151357/9e8e01df4ee12b1f04cd61e0ecee3ea8bd6f617b/mit-license.txt
 """
-import json, math, subprocess, sys, urllib2
+import json, math, string, subprocess, sys, urllib2
 
 
 def main(city, country):
     url = ('http://api.openweathermap.org/data/2.5/weather?q='+ city +','
            + country)
     data = json.loads(urllib2.urlopen(url).read())
-    temperature = math.ceil((data['main']['temp'] - 273.15) * 100) / 100
-    print(data['weather'][0]['description']+ ", "+ str(temperature) +" degr.")
+    description = string.lower(data['weather'][0]['description'])
+    temperature = str(math.ceil((data['main']['temp'] - 273.15) * 100) / 100)
+    print(description + ", "+ temperature +" deg.")
 
 
 if __name__ == "__main__":
