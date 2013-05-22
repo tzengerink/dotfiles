d612888bb63179db2886719823cee09fa5420b13
T. Zengerink <t.zengerink@gmail.com>
2013-05-22 09:35:50 +0200

    improved weather command

--- .bin/weather.py
+++ .bin/weather.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
     WEATHER
     -------
     Get the weather in a given location
+
+    Example usage: `weather.py Amsterdam Netherlands`
+
+    Copyright (c) 2012-2013 T. Zengerink
+    Licensed under MIT License
+    See: https://raw.github.com/gist/3151357/9e8e01df4ee12b1f04cd61e0ecee3ea8bd6f617b/mit-license.txt
 """
-import json
-import math
-import urllib.request
-import sys
+import json, math, subprocess, sys, urllib2
 
 
-def main(location):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q='+ location
-    data = urllib.request.urlopen(url).read()
-    obj = json.loads(str(data, "utf8"))
-    description = obj['weather'][0]['description']
-    temperature = math.ceil((obj['main']['temp'] - 273.15) * 100) / 100
-    print(description+ ", "+ str(temperature) +" degr.")
+def main(city, country):
+    url = ('http://api.openweathermap.org/data/2.5/weather?q='+ city +','
+           + country)
+    data = json.loads(urllib2.urlopen(url).read())
+    temperature = math.ceil((data['main']['temp'] - 273.15) * 100) / 100
+    print(data['weather'][0]['description']+ ", "+ str(temperature) +" degr.")
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    if len(sys.argv) < 2:
+        print("Usage: "+ sys.argv[0] +" [city] [country]")
+        exit(1)
+    main(sys.argv[1], sys.argv[2])
