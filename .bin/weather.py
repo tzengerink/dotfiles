ad194a3967f13145af8563e286f221cc44e07ac8
T. Zengerink <t.zengerink@gmail.com>
2013-06-19 08:51:24 +0200

    caching weather data

--- .bin/weather.py
+++ .bin/weather.py
@@ -18,20 +18,65 @@ import json, math, sys, time, urllib2
 API_KEY = '45f08410c9003006ff4287e1b3d7f0ec'
 
 
+class File:
+    """Assists in handling files.
+    path -- Path of the file.
+    """
+    def __init__(self, path):
+        self.path = path
+
+    def clear(self):
+        """Clear the contents of the file."""
+        open(self.path, 'w').close()
+
+    def read(self):
+        """Read the contents of the file."""
+        contents = ""
+        handle = open(self.path)
+        while 1:
+            line = handle.readline()
+            if not line:
+                break
+            contents += line
+        handle.close()
+        return contents
+
+    def write(self, txt):
+        """Append text to the end of the file.
+        txt -- Text to append to the file.
+        """
+        handle = open(self.path, 'a')
+        handle.write(txt)
+        handle.close()
+
+
 def fahrenheit2celcius(i):
     return math.ceil((((i - 32) * 5) / 9) * 100) / 100
 
 def get_point(data, point, timing='currently'):
     return data[timing][point]
 
-def main(args):
+def get_data(args):
     lat_long = '%s,%s' % (args[0], args[1])
     url = 'https://api.forecast.io/forecast/%s/%s' % (API_KEY, lat_long)
     data = json.loads(urllib2.urlopen(url).read())
     summary = get_point(data, 'summary')
     temperature = fahrenheit2celcius(get_point(data, 'temperature'))
-    print("%s, %s" % (summary, str(temperature)))
+    return '%s, %s' % (summary, str(temperature))
 
+def main(args):
+    cache = File('/tmp/weather-%s.%s' % (args[0], args[1]))
+    lifetime = 600
+    try:
+        data = json.loads(cache.read())
+        if time.time() > data['expires']:
+            raise Exception('Cache expired')
+    except:
+        data = {"content": get_data(args),
+                "expires": time.time() + lifetime}
+        cache.clear()
+        cache.write(json.dumps(data))
+    print(data['content'])
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
