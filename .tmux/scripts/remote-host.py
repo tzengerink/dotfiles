cf8f2c3ee4deb29fa35c050e0f4cfd5f8e3fb8f0
T. Zengerink <t.zengerink@gmail.com>
2013-11-05 14:14:40 +0100

    refactered script for better readability

--- .tmux/scripts/remote-host.py
+++ .tmux/scripts/remote-host.py
@@ -10,74 +10,48 @@
     Licensed under MIT License
     See: https://raw.github.com/gist/3151357/9e8e01df4ee12b1f04cd61e0ecee3ea8bd6f617b/mit-license.txt
 """
-import argparse, os, subprocess
+import argparse, os
 
 
-FILE_PATH = '/tmp/tmux-remote-host'
-
-
-class File:
-    """Assists in handling files.
-    path -- Path of the file.
-    """
-    def __init__(self, path):
-        self.path = path
-
-    def clear(self):
-        """Clear the contents of the file."""
-        open(self.path, 'w').close()
-
-    def read(self):
-        """Read the contents of the file."""
-        contents = ""
-        handle = open(self.path)
-        while 1:
-            line = handle.readline()
-            if not line:
-                break
-            contents += line
-        handle.close()
-        return contents
-
-    def write(self, txt):
-        """Append text to the end of the file.
-        txt -- Text to append to the file.
-        """
-        handle = open(self.path, 'a')
-        handle.write(txt)
-        handle.close()
+class RemoteHost:
+    def __init__(self, file_path='/tmp/tmux-remote-host'):
+        self.file_path = file_path
+    def get_hostname(self, format='%s'):
+        try:
+            with open(self.file_path, 'r') as file:
+                return format % str(file.readline()).strip()
+        except:
+            return ''
+    def set_hostname(self, hostname):
+        os.system('echo "%s" > %s' % (hostname, self.file_path))
+    def get_ssh_command(self):
+        hostname = self.get_hostname()
+        return 'ssh %s' % hostname if hostname else ''
+    def switch_hostname(self):
+        os.system("tmux display-message -p '#S' > %s" % self.file_path)
 
 
 def main(args):
-    file = File(FILE_PATH)
-    hostname = str(file.read()).strip()
-    if args.switch:
-        file.clear()
-        os.system("tmux display-message -p '#S' > %s" % FILE_PATH)
+    remote = RemoteHost()
+    if args.command:
+        print(remote.get_ssh_command())
         exit(0)
     if args.display:
-        if hostname:
-            print(' %s ' % hostname)
-        else:
-            print('')
+        print(remote.get_hostname(' %s '))
         exit(0)
-    if args.command:
-        if hostname:
-            print('ssh %s' % hostname)
+    if args.switch:
+        remote.switch_hostname()
         exit(0)
-    if args.host:
-        file.clear()
-        file.write(args.host)
-    else:
-        file.clear()
+    remote.set_hostname(args.host)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--display', action='store_true', default=False,
-                        dest='display')
-    parser.add_argument('--command', action='store_true', default=False,
+    parser.add_argument('-c', '--command', action='store_true', default=False,
                         dest='command')
-    parser.add_argument('--switch', action='store_true', default=False,
+    parser.add_argument('-d', '--display', action='store_true', default=False,
+                        dest='display')
+    parser.add_argument('-s', '--switch', action='store_true', default=False,
                         dest='switch')
-    parser.add_argument('host', default=False, nargs='?')
+    parser.add_argument('host', default='', nargs='?')
     main(parser.parse_args())
