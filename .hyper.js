1a2cf23b7d7868f121c0c372d1f039e85773aae1
Teun Zengerink <t.zengerink@gmail.com>
2021-01-26 13:06:59 +0100

    Updated configuration files

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyper-highlight-active-pane"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
