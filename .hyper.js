446750af75ddff99ff56f7ecb35c1b8ff36d307e
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2022-02-14 13:34:46 +0100

    Configuration changes for hyper & vscode

--- .hyper.js
+++ .hyper.js
@@ -171,7 +171,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyper-firefox-devtools"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
