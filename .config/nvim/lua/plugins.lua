d366e8cf846377359511a766f1fac5afeab34eb0
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2023-09-11 17:26:14 +0200

    Added installation instructions NeoVim packer

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,6 +1,8 @@
 -- To install packer run:
 --
---   git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+--   $> git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+--   $> nvim
+--   :PackerInstall
 --
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
