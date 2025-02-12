1129500764aec87181efc3982f84c0e4253b89b1
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-02-12 13:46:36 +0100

    Load GitHub copilot aliases in seperate file

--- .zsh/aliases.sh
+++ .zsh/aliases.sh
@@ -4,14 +4,10 @@ alias -g T='| tail'
 alias -g J='| python3 -mjson.tool'
 alias -g C='| pbcopy'
 
-# git / github
-alias g='git'
-alias ce='gh copilot explain'
-alias cs='gh copilot suggest'
-
 # tools
 alias ls='lsd --group-dirs=first'
 alias ll='ls -glA'
+alias g='git'
 alias t="tmux new-session -A -s main"
 alias n='nvim'
 alias f='fzf'
