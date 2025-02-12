0ff3387cafa6628834cf7fd3f5640e58ee1c474e
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-02-12 13:54:44 +0100

    Improve change-dir.sh functionality

--- .zsh/change-dir.sh
+++ .zsh/change-dir.sh
@@ -1,4 +1,4 @@
-c() {
+change_dir() {
   local search_dir=${1:-~}
   local max_depth=${2:-3}
 
@@ -6,3 +6,5 @@ c() {
   dir=$(find "$search_dir" -maxdepth "$max_depth" -type d 2> /dev/null | fzf --height=20 --reverse --preview 'tree -C {}' --preview-window=right:60%) &&
   cd "$dir"
 }
+
+bindkey -s '^n' 'change_dir\n'
