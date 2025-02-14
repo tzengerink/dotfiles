80a570e7d677169fdc90834d929bf0395548a384
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-02-14 09:58:49 +0100

    Update prompt to match tmux status bar

--- .zsh/prompt.sh
+++ .zsh/prompt.sh
@@ -1,4 +1,4 @@
-local prompt_user='%n'
+local prompt_user='%n  '
 local prompt_dir='$(pre_prompt_dir)'
 local prompt_newline='$(pre_prompt_newline)'
 local prompt_node='$(pre_prompt_node)'
@@ -7,8 +7,8 @@ local prompt_shell='$(pre_prompt_shell)'
 local prompt_exit='$(pre_prompt_exit)'
 
 function pre_prompt_exit {
-	local SUCCESS="%{$fg[green]%}%j%{$reset_color%}"
-	local ERROR="%{$fg[red]%}%j%{$reset_color%}"
+	local SUCCESS="%{$fg[green]%}%j  %{$reset_color%}"
+	local ERROR="%{$fg[red]%}%j  %{$reset_color%}"
 	echo -e "%(?.$SUCCESS.$ERROR)"
 }
 
@@ -18,15 +18,15 @@ function pre_prompt_repo {
 	if [[ -d ".git" ]]; then
 		local BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
     if [[ $BRANCH =~ ^(master|main)$ ]]; then
-			local BRANCH="%{$fg[red]%}$BRANCH%{$reset_color%}"
+			local BRANCH="%{$fg[red]%}$BRANCH  %{$reset_color%}"
 		fi
     if [[ $BRANCH =~ ^develop$ ]]; then
-			local BRANCH="%{$fg[yellow]%}$BRANCH%{$reset_color%}"
+			local BRANCH="%{$fg[yellow]%}$BRANCH  %{$reset_color%}"
 		fi
     if [[ $BRANCH =~ ^(feature|feat|refactor|chore|fix|docs)\/ ]]; then
-			local BRANCH="%{$fg[blue]%}$BRANCH%{$reset_color%}"
+			local BRANCH="%{$fg[blue]%}$BRANCH  %{$reset_color%}"
 		fi
-		echo -e "·$BRANCH"
+		echo -e "$BRANCH"
 	else
 		echo ""
 	fi
@@ -34,7 +34,7 @@ function pre_prompt_repo {
 }
 
 function pre_prompt_dir {
-	echo -e "%{$reset_color%}·%{$fg[magenta]%}$(shrink_path -f)%{$reset_color%}"
+	echo -e "%{$reset_color%}%{$fg[magenta]%}$(shrink_path -f)  %{$reset_color%}"
 }
 
 function pre_prompt_newline {
@@ -43,8 +43,8 @@ function pre_prompt_newline {
 
 function pre_prompt_node {
 	if [[ -d "node_modules" ]] && which node >/dev/null; then
-		echo -e "%{$reset_color%}·%F%{$fg[yellow]%}$(node --version)%f%{$reset_color%}"
+		echo -e "%{$reset_color%}%{$fg[yellow]%}$(node --version)  %{$reset_color%}"
 	fi
 }
 
-export PROMPT="${prompt_user}${prompt_dir}${prompt_repo}${prompt_node}${prompt_newline}${prompt_exit}·> "
+export PROMPT="${prompt_user}${prompt_dir}${prompt_repo}${prompt_node}${prompt_newline}${prompt_exit}"
