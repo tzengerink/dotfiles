e6eb689dfd96e5fce8f75c44ed267b9274a13729
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-02-17 18:12:06 +0100

    Remove unnecessary alias

--- .zsh/fzf.sh
+++ .zsh/fzf.sh
@@ -5,5 +5,3 @@ export FZF_DEFAULT_OPTS="--layout=default --walker-skip .git,node_modules,dist,c
 export FZF_CTRL_R_OPTS="--height=12"
 export FZF_CTRL_T_OPTS="--tmux center,90%,90% --preview 'bat --color=always --style=numbers,changes --line-range :500 {}'"
 export FZF_ALT_C_OPTS="--layout=reverse --preview 'tree -C {}'"
-
-alias f='fzf'
