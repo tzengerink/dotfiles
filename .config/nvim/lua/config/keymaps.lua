f16366ecaeb0f6a09ca1327da8ced18563a2f46d
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-02-27 11:08:18 +0100

    Configured improved auto complete

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap('n', '<C-l>', ':tabnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-h>', ':tabprevious<CR>', { noremap = true, silent = true })
