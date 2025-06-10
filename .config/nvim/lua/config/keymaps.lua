00a28daaef6a83b2a9edda0d1b05df856fba6f8c
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-06-10 11:25:41 +0200

    Configuration adjustments

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,9 @@
+-- Tabs
 vim.api.nvim_set_keymap('n', '<C-l>', ':tabnext<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<C-h>', ':tabprevious<CR>', { noremap = true, silent = true })
+
+-- Window resize
+vim.api.nvim_set_keymap('n', '<C-w><C-Up>', '5<C-w>+', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w><C-Down>', '5<C-w>-', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w><C-Left>', '5<C-w><', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w><C-Right>', '5<C-w>>', { noremap = true, silent = true })
