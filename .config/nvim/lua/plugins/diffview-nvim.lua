19462396b2ffac769e0a92254266021c9e209272
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-08-14 10:35:02 +0200

    Switch to tokyonight color scheme

--- .config/nvim/lua/plugins/diffview-nvim.lua
+++ .config/nvim/lua/plugins/diffview-nvim.lua
@@ -5,6 +5,7 @@ return {
       { '<LEADER>gd', '<cmd>DiffviewOpen<CR>', desc = 'Open Diffview' },
       { '<LEADER>gD', '<cmd>DiffviewClose<CR>', desc = 'Close Diffview' },
       { '<LEADER>gh', '<cmd>DiffviewFileHistory<CR>', desc = 'File History' },
+      { '<LEADER>gH', '<cmd>DiffviewOpen HEAD~1<CR>', desc = 'Open Diffview HEAD~1' },
     },
     config = function()
       require('diffview').setup({
