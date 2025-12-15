29133a8d784cb149a1e451b69b085eefc27e2e3c
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-12-15 15:27:19 +0100

    Revert aider changes

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -4,11 +4,11 @@ return {
     enabled = true,
     lazy = false,
     keys = {
-      { '<C-p>', ':Gitsigns preview_hunk<CR>', { noremap = true, silent = true } },
+      --{ '<C-p>', ':Gitsigns preview_hunk<CR>', { noremap = true, silent = true } },
       { '<C-a>', ':Gitsigns stage_hunk<CR>', { noremap = true, silent = true } },
       { '<C-u>', ':Gitsigns reset_hunk<CR>', { noremap = true, silent = true } },
-      { '<C-j>', ':Gitsigns next_hunk<CR>', { noremap = true, silent = true } },
-      { '<C-k>', ':Gitsigns prev_hunk<CR>', { noremap = true, silent = true } },
+      --{ '<C-j>', ':Gitsigns next_hunk<CR>', { noremap = true, silent = true } },
+      --{ '<C-k>', ':Gitsigns prev_hunk<CR>', { noremap = true, silent = true } },
     },
     config = function()
       require('gitsigns').setup({
--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -1,1 +1,43 @@
--- configure fim for qwen2.5-coder models AI!
+return {
+  {
+    'github/copilot.vim',
+    enabled = false,
+    init = function()
+      vim.g.copilot_no_tab_map = true
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false } )
+    end,
+  },
+  {
+    'preservim/nerdcommenter',
+    event = "VeryLazy",
+  },
+  {
+    "chentoast/marks.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "folke/zen-mode.nvim",
+    event = "VeryLazy",
+    keys = {
+      {
+        "<LEADER>z",
+        function()
+          require("zen-mode").toggle()
+        end,
+        desc = "Toggle Zen Mode",
+      },
+    },
+    opts = {
+      window = {
+        width = 120,
+        height = 0.97,
+      },
+      plugins = {
+        tmux = {
+          enabled = true,
+        },
+      },
+    },
+  },
+}
