b3cf206da805c41cf78e6db3cef6e7aea71a03cd
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-12-16 13:10:31 +0100

    Add python prompt part again

--- .zsh/prompt.sh
+++ .zsh/prompt.sh
@@ -2,6 +2,7 @@ local prompt_user='$(pre_prompt_user)'
 local prompt_dir='$(pre_prompt_dir)'
 local prompt_newline='$(pre_prompt_newline)'
 local prompt_node='$(pre_prompt_node)'
+local prompt_python='$(pre_prompt_python)'
 local prompt_repo='$(pre_prompt_branch)'
 local prompt_shell='$(pre_prompt_shell)'
 local prompt_exit='$(pre_prompt_exit)'
@@ -20,13 +21,13 @@ function pre_prompt_branch {
 	local BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 	if [[ -n $BRANCH ]]; then
 		if [[ $BRANCH =~ ^(master|main)$ ]]; then
-			echo -e "%{$fg[red]%}$BRANCH  %{$reset_color%}"
+			echo -e "%{$fg[red]%}$BRANCH  %{$reset_color%}"
 		elif [[ $BRANCH =~ ^develop$ ]]; then
-			echo -e "%{$fg[yellow]%}$BRANCH  %{$reset_color%}"
+			echo -e "%{$fg[yellow]%}$BRANCH  %{$reset_color%}"
 		elif [[ $BRANCH =~ ^(feature|feat|refactor|chore|fix|docs)\/ ]]; then
-			echo -e "%{$fg[blue]%}$BRANCH  %{$reset_color%}"
+			echo -e "%{$fg[blue]%}$BRANCH  %{$reset_color%}"
 		else
-			echo -e "%{$fg[white]%}$BRANCH  %{$reset_color%}"
+			echo -e "%{$fg[white]%}$BRANCH  %{$reset_color%}"
 		fi
 	fi
 }
@@ -39,10 +40,16 @@ function pre_prompt_newline {
 	echo -e "%b\n%b"
 }
 
+function pre_prompt_python {
+	if [[ -d ".venv" ]] && which python >/dev/null; then
+		echo -e "%{$reset_color%}%{$fg[yellow]%}$(python --version)  %{$reset_color%}"
+	fi
+}
+
 function pre_prompt_node {
 	if [[ -d "node_modules" ]] && which node >/dev/null; then
 		echo -e "%{$reset_color%}%{$fg[yellow]%}$(node --version)  %{$reset_color%}"
 	fi
 }
 
-export PROMPT="${prompt_user}${prompt_dir}${prompt_repo}${prompt_node}${prompt_newline}${prompt_exit}"
+export PROMPT="${prompt_user}${prompt_dir}${prompt_repo}${prompt_node}${prompt_python}${prompt_newline}${prompt_exit}"
