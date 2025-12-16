d3cc1aab79e23396504f7e1f9390ec2c83b9f47a
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-12-16 16:06:35 +0100

    Add auto formatting using conform.vim

--- .config/nvim/lua/plugins/language.lua
+++ .config/nvim/lua/plugins/language.lua
@@ -91,7 +91,23 @@ return {
     end,
   },
   {
+    'stevearc/conform.nvim',
+    opts = {
+      formatters_by_ft = {
+         javascript = { "prettier" },
+         typescript = { "prettier" },
+        python = { "isort", "black" },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    },
+  },
+  {
     'prettier/vim-prettier',
+    enabled = false,
     config = function()
       vim.cmd([[
         let g:prettier#config#trailing_comma = 'all'
