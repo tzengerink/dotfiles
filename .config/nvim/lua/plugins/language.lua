df6e66196737fc0c308c12a411c5e307933a4bd4
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2025-12-17 08:15:56 +0100

    Add instructions for coc-pyright

--- .config/nvim/lua/plugins/language.lua
+++ .config/nvim/lua/plugins/language.lua
@@ -4,6 +4,7 @@ return {
     branch = 'release',
     config = function()
       -- :CocInstall coc-tsserver
+      -- :CocInstall coc-pyright
       function _G.check_back_space()
         local col = vim.fn.col('.') - 1
         return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
