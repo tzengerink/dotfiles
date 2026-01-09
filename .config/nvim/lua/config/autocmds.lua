9e05ac4ab5fb9a3d6e5b18e48584157cd1e9956a
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2026-01-09 15:16:32 +0100

    Improve configurations

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -17,5 +17,6 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.tabstop = 2
     vim.opt_local.shiftwidth = 2
     vim.opt_local.expandtab = false
+    vim.opt_local.wrap = true
   end
 })
