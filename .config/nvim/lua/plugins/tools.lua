85fbdee16edaeceed67e23b227ea510cd71f595f
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2026-01-15 10:04:32 +0100

    Display current working dir in status bar

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -22,7 +22,6 @@ return {
   },
   {
     'github/copilot.vim',
-    enabled = false,
     init = function()
       vim.g.copilot_no_tab_map = true
       vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false } )
