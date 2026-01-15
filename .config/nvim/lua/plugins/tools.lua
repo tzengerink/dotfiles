dc7e364317b89e47fa8e0ded542137d98290d168
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2026-01-15 14:56:18 +0100

    Configure AI assistance

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -24,7 +24,7 @@ return {
     'github/copilot.vim',
     init = function()
       vim.g.copilot_no_tab_map = true
-      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false } )
+      vim.keymap.set('i', '<C-L>', 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false } )
     end,
   },
   {
