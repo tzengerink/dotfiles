4a65d4b7efec58cd6f94d7ebe7320d4bce85b4fd
Teun Zengerink <539075+tzengerink@users.noreply.github.com>
2026-01-29 14:22:53 +0100

    Add icons for the prompt parts

--- .zsh/prompt.sh
+++ .zsh/prompt.sh
@@ -22,13 +22,13 @@ function pre_prompt_branch {
 	local BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 	if [[ -n $BRANCH ]]; then
 		if [[ $BRANCH =~ ^(master|main)$ ]]; then
-      echo -e "%{$fg[red]%}%{$terminfo[smul]%}$BRANCH%{$terminfo[rmul]%}  %{$reset_color%}"
+      echo -e "%{$fg[red]%}%{$terminfo[smul]%} $BRANCH%{$terminfo[rmul]%}  %{$reset_color%}"
 		elif [[ $BRANCH =~ ^develop$ ]]; then
-			echo -e "%{$fg[yellow]%}$BRANCH  %{$reset_color%}"
+			echo -e "%{$fg[yellow]%} $BRANCH  %{$reset_color%}"
 		elif [[ $BRANCH =~ ^(feature|feat|refactor|chore|fix|docs)\/ ]]; then
-			echo -e "%{$fg[blue]%}$BRANCH  %{$reset_color%}"
+			echo -e "%{$fg[blue]%} $BRANCH  %{$reset_color%}"
 		else
-			echo -e "%{$fg[white]%}$BRANCH  %{$reset_color%}"
+			echo -e "%{$fg[white]%} $BRANCH  %{$reset_color%}"
 		fi
 	fi
 }
@@ -43,7 +43,7 @@ function pre_prompt_newline {
 
 function pre_prompt_python {
 	if [[ -d ".venv" ]] && which python >/dev/null; then
-		echo -e "%{$reset_color%}%{$fg[yellow]%}$(python --version)  %{$reset_color%}"
+		echo -e "%{$reset_color%}%{$fg[yellow]%}󰌠 $(python --version)  %{$reset_color%}"
 	fi
 }
 
@@ -51,18 +51,18 @@ function pre_prompt_kubectx {
 	if command -v kubectl >/dev/null 2>&1; then
 		local KUBE_CONTEXT=$(kubectl config current-context 2>/dev/null)
 		if [[ $KUBE_CONTEXT =~ ^prd$ ]]; then
-      echo -e "%{$fg[red]%}%{$terminfo[smul]%}$KUBE_CONTEXT%{$terminfo[rmul]%}  %{$reset_color%}"
+      echo -e "%{$fg[red]%}%{$terminfo[smul]%}⚓$KUBE_CONTEXT%{$terminfo[rmul]%}  %{$reset_color%}"
 		elif [[ $KUBE_CONTEXT =~ ^dev$ ]]; then
-			echo -e "%{$fg[green]%}$KUBE_CONTEXT  %{$reset_color%}"
+			echo -e "%{$fg[green]%}⚓$KUBE_CONTEXT  %{$reset_color%}"
 		elif [[ -n $KUBE_CONTEXT ]]; then
-			echo -e "%{$fg[cyan]%}$KUBE_CONTEXT  %{$reset_color%}"
+			echo -e "%{$fg[cyan]%}⚓$KUBE_CONTEXT  %{$reset_color%}"
 		fi
 	fi
 }
 
 function pre_prompt_node {
 	if [[ -d "node_modules" ]] && which node >/dev/null; then
-		echo -e "%{$reset_color%}%{$fg[yellow]%}$(node --version)  %{$reset_color%}"
+		echo -e "%{$reset_color%}%{$fg[yellow]%}󰎙 $(node --version)  %{$reset_color%}"
 	fi
 }
 
